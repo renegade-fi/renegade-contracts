74fa2ce14c2f0ce6db405edb3699c56949b8ab83
Joey Kraut <joey@renegade.fi>
2022-12-28 08:21:10 -0800

    contracts: merkle: Add insert method to Merkle tree

--- tests/test_merkle.py
+++ tests/test_merkle.py
@@ -4,6 +4,8 @@ Groups tests for the Merkle tree implementation
 import os
 import pytest
 
+from typing import List
+
 from nile.utils import assert_revert
 
 from starkware.starknet.testing.starknet import Starknet
@@ -17,11 +19,15 @@ EMPTY_LEAF_VAL = (
     306932273398430716639340090025251549301604242969558673011416862133942957551
 )
 # The height of the Merkle tree used for testing
-MERKLE_HEIGHT = 32
+MERKLE_HEIGHT = 10
 # The path to the Merkle tree contract source
 MERKLE_FILE = os.path.join("contracts", "merkle", "Merkle.cairo")
 # The number of historical roots stored by the Merkle contract
 ROOT_HISTORY_LEN = 30
+
+############
+# Fixtures #
+############
 
 
 @pytest.fixture(scope="module")
@@ -33,6 +39,31 @@ async def merkle_contract(starknet_state: Starknet) -> StarknetContract:
     await merkle_contract.initializer(height=MERKLE_HEIGHT).execute()
 
     return merkle_contract
+
+
+###########
+# Helpers #
+###########
+
+
+def assert_power_of_2(n: int):
+    """
+    Asserts that the input is a power of 2
+    """
+    assert n & (n - 1) == 0 and n > 0
+
+
+def compute_merkle_root(leaves: List[int]) -> int:
+    """
+    Computes the merkle root of the given
+    """
+    assert_power_of_2(len(leaves))
+
+    res = leaves
+    while len(res) > 1:
+        res = [pedersen_hash(res[i], res[i + 1]) for i in range(0, len(res), 2)]
+
+    return res[0]
 
 
 class TestMerkle:
@@ -74,3 +105,21 @@ class TestMerkle:
             merkle_contract.get_root(index=ROOT_HISTORY_LEN).call(),
             "root index must be within history length",  # Expected substring
         )
+
+    @pytest.mark.asyncio
+    async def test_single_insert(self, merkle_contract: StarknetContract):
+        """
+        Tests that inserts into the tree update the root properly
+        """
+        # Compute the expected root of the first insert into the tree
+        insert_value = 42
+        expected_root = insert_value
+
+        n_elems = 2**MERKLE_HEIGHT
+        leaf_data = [insert_value] + [EMPTY_LEAF_VAL] * (n_elems - 1)
+
+        expected_root = compute_merkle_root(leaf_data)
+
+        # Insert the value into the contract and check the root
+        exec_info = await merkle_contract.insert(value=insert_value).execute()
+        assert exec_info.result == (expected_root,)
