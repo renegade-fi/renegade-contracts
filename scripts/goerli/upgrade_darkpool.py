4d7740cb532614176cb01349987c3766163176d8
Joey Kraut <joey@renegade.fi>
2023-02-27 15:55:16 -0800

    scripts: goerli: Add implementation class upgrade logic

---
+++ scripts/goerli/upgrade_darkpool.py
@@ -0,0 +1,61 @@
+"""
+Declares a new darkpool implementation and switches the implementation pointer in the proxy contract
+"""
+
+import logging
+
+from nile.common import ETH_TOKEN_ADDRESS
+from nile.core.account import Account
+from nile.nre import NileRuntimeEnvironment
+from nile.utils import to_uint, hex_address
+from starkware.starknet.public.abi import get_selector_from_name
+
+"""
+Constants
+"""
+
+DARKPOOL_CONTRACT_NAME = "Darkpool"
+MERKLE_CONTRACT_NAME = "Merkle"
+NULLIFIER_CONTRACT_NAME = "NullifierSet"
+PROXY_CONTRACT_NAME = "Proxy"
+MAX_FEE = 10**15
+
+"""
+Script
+"""
+
+
+async def run(nre: NileRuntimeEnvironment):
+    """
+    Declares a new Darkpool implementation and upgrades the contract
+    """
+    logging.getLogger().setLevel(logging.DEBUG)
+    logging.getLogger().disabled = False
+
+    # Find a set of pre-deployed accounts
+    accounts = await nre.get_accounts()
+    account: Account = accounts[0]
+
+    account_nonce = await nre.get_nonce(contract_address=account.address)
+
+    # Declare the darkpool implementation
+    (darkpool_hash, _) = await account.declare(
+        DARKPOOL_CONTRACT_NAME,
+        alias=DARKPOOL_CONTRACT_NAME,
+        nonce=account_nonce,
+        max_fee=MAX_FEE,
+        watch_mode="track",
+    )
+    account_nonce += 1
+
+    # Upgrade the darkpool in the proxy
+    await account.send(
+        PROXY_CONTRACT_NAME,
+        "upgrade",
+        [darkpool_hash],
+        nonce=account_nonce,
+        max_fee=MAX_FEE,
+        watch_mode="track",
+    )
+
+    logging.info("\n🕺 Darkpool upgraded!")
---
+++ scripts/goerli/upgrade_nullifier.py
@@ -0,0 +1,62 @@
+"""
+Upgrades the nullifier set implementation in the contract
+"""
+
+import logging
+
+from nile.common import ETH_TOKEN_ADDRESS
+from nile.core.account import Account
+from nile.nre import NileRuntimeEnvironment
+from nile.utils import to_uint, hex_address
+from starkware.starknet.public.abi import get_selector_from_name
+
+"""
+Constants
+"""
+
+DARKPOOL_CONTRACT_NAME = "Darkpool"
+MERKLE_CONTRACT_NAME = "Merkle"
+NULLIFIER_CONTRACT_NAME = "NullifierSet"
+PROXY_CONTRACT_NAME = "Proxy"
+MAX_FEE = 10**15
+
+"""
+Script
+"""
+
+
+async def run(nre: NileRuntimeEnvironment):
+    """
+    Declares the new nullifier set implementation class and then upgrades the
+    pointer in the contract
+    """
+    logging.getLogger().setLevel(logging.DEBUG)
+    logging.getLogger().disabled = False
+
+    # Find a set of pre-deployed accounts
+    accounts = await nre.get_accounts()
+    account: Account = accounts[0]
+
+    account_nonce = await nre.get_nonce(contract_address=account.address)
+
+    # Declare the new Merkle implementation
+    (declared_hash, _) = await account.declare(
+        NULLIFIER_CONTRACT_NAME,
+        alias=NULLIFIER_CONTRACT_NAME,
+        nonce=account_nonce,
+        max_fee=MAX_FEE,
+        watch_mode="track",
+    )
+    account_nonce += 1
+
+    # Call the upgrade method on the new implementation hash
+    await account.send(
+        PROXY_CONTRACT_NAME,
+        "upgrade_nullifier",
+        [declared_hash],
+        nonce=account_nonce,
+        max_fee=MAX_FEE,
+        watch_mode="track",
+    )
+
+    logging.info("\n🕺 Nullifier implementation upgraded!")
