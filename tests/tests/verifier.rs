37fdf5c7d38e3bbf47c31e347550a3f7f450c99f
Andrew Kirillov <20803092+akirillo@users.noreply.github.com>
2023-08-08 15:41:31 -0700

    tests: verifier: implemented invalid proof test

--- tests/tests/verifier.rs
+++ tests/tests/verifier.rs
@@ -1,4 +1,6 @@
 use eyre::Result;
+use mpc_stark::algebra::scalar::Scalar;
+use rand::thread_rng;
 use tests::{
     utils::{global_teardown, random_felt},
     verifier::utils::{
@@ -28,6 +30,34 @@ async fn test_full_verification_fuzz() -> Result<()> {
             .await?
             .unwrap());
     }
+
+    global_teardown(sequencer);
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_full_verification_invalid_proof() -> Result<()> {
+    let sequencer = setup_verifier_test().await?;
+    let account = sequencer.account();
+
+    let (mut proof, witness_commitments) = singleprover_prove_dummy_circuit()?;
+    // Fuzz a part of the proof so it becomes invalid
+    proof.t_x = Scalar::random(&mut thread_rng());
+    let verification_job_id = random_felt();
+    queue_verification_job(&account, &proof, &witness_commitments, verification_job_id).await?;
+
+    while check_verification_job_status(&account, verification_job_id)
+        .await?
+        .is_none()
+    {
+        step_verification(&account, verification_job_id).await?;
+    }
+
+    assert!(
+        !check_verification_job_status(&account, verification_job_id)
+            .await?
+            .unwrap()
+    );
 
     global_teardown(sequencer);
     Ok(())
