7ba58139ccce66996377b9ab680a9348a9f41173
Andrew Kirillov <andrew@kirillov.org>
2023-10-09 20:58:05 -0700

    verifier, types: initialize Verifier smart contract w/ ABI-compatible storage types

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,9 @@
 mod constants;
 mod transcript;
 mod types;
+mod verifier;
 
 extern crate alloc;
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-use alloc::vec::Vec;
-
-use stylus_sdk::stylus_proc::entrypoint;
-
-#[entrypoint]
-fn user_main(input: Vec<u8>) -> Result<Vec<u8>, Vec<u8>> {
-    Ok(input)
-}
