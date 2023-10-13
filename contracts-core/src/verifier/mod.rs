a9802d659232bd1ea1a035eeffcf6416f50e8f05
Andrew Kirillov <andrew@kirillov.org>
2023-10-13 15:01:07 -0700

    hardcoded wires & selectors

--- contracts-core/src/verifier/mod.rs
+++ contracts-core/src/verifier/mod.rs
@@ -13,26 +13,47 @@ use ark_poly::{EvaluationDomain, Radix2EvaluationDomain};
 use core::result::Result;
 
 use crate::{
-    constants::NUM_WIRE_TYPES,
     transcript::{Transcript, TranscriptHasher},
     types::{Challenges, G1Affine, G2Affine, Proof, ScalarField, VerificationKey},
 };
 
 use self::errors::VerifierError;
 
-pub trait G1EcArithmetic {
+/// Encapsulates the implementations of elliptic curve arithmetic done on the G1 source group,
+/// including a pairing identity check with elements of the G2 source group.
+/// 
+/// The type that implements this trait should be a unit struct that either calls out to precompiles
+/// for EC arithmetic and pairings in a smart contract context, or call out to Arkworks code in a testing context.
+pub trait G1ArithmeticBackend {
+    /// Add two points in G1
     fn ec_add(a: G1Affine, b: G1Affine) -> Result<G1Affine, VerifierError>;
+    /// Multiply a G1 point by a scalar in its scalar field
     fn ec_scalar_mul(a: ScalarField, b: G1Affine) -> Result<G1Affine, VerifierError>;
+    /// Check the pairing identity e(a_1, b_1) == e(a_2, b_2)
     fn ec_pairing_check(
         a_1: G1Affine,
         b_1: G2Affine,
         a_2: G1Affine,
         b_2: G2Affine,
     ) -> Result<bool, VerifierError>;
+
+    /// A helper for computing multi-scalar multiplications over G1
+    fn msm(scalars: &[ScalarField], points: &[G1Affine]) -> Result<G1Affine, VerifierError> {
+        if scalars.len() != points.len() {
+            return Err(VerifierError::MsmLength);
+        }
+
+        scalars
+            .iter()
+            .zip(points.iter())
+            .try_fold(G1Affine::identity(), |acc, (scalar, point)| {
+                Self::ec_add(acc, Self::ec_scalar_mul(*scalar, *point)?)
+            })
+    }
 }
 
 /// Verify a proof. Follows the algorithm laid out in section 8.3 of the paper: https://eprint.iacr.org/2019/953.pdf
-pub fn verify<H: TranscriptHasher, G: G1EcArithmetic>(
+pub fn verify<H: TranscriptHasher, G: G1ArithmeticBackend>(
     vkey: &VerificationKey,
     proof: &Proof,
     public_inputs: &[ScalarField],
@@ -57,7 +78,7 @@ pub fn verify<H: TranscriptHasher, G: G1EcArithmetic>(
 
     let zero_poly_eval_div_n = ScalarField::from(vkey.n)
         .inverse()
-        .ok_or(VerifierError::InversionError)?
+        .ok_or(VerifierError::Inversion)?
         * zero_poly_eval;
 
     let lagrange_1_eval = step_6(zero_poly_eval_div_n, &domain, &challenges);
@@ -74,9 +95,20 @@ pub fn verify<H: TranscriptHasher, G: G1EcArithmetic>(
 
     let d_1 = step_9::<G>(zero_poly_eval, lagrange_1_eval, vkey, proof, &challenges)?;
 
-    let f_1 = step_10::<G>(d_1, vkey, proof, &challenges)?;
+    let v_2 = challenges.v * challenges.v;
+    let v_4 = v_2 * v_2;
+    let v_powers = [
+        ScalarField::one(),
+        challenges.v,
+        v_2,
+        challenges.v * v_2,
+        v_4,
+        challenges.v * v_4,
+    ];
 
-    let neg_e_1 = step_11::<G>(r_0, vkey, &challenges, proof)?;
+    let f_1 = step_10::<G>(d_1, &v_powers, vkey, proof)?;
+
+    let neg_e_1 = step_11::<G>(r_0, &v_powers, vkey, proof, &challenges)?;
 
     step_12::<G>(f_1, neg_e_1, &domain, vkey, proof, &challenges)
 }
@@ -107,7 +139,9 @@ fn step_4<H: TranscriptHasher>(
 
 /// Evaluate the zero polynomial at the challenge point `zeta`
 fn step_5(domain: &Radix2EvaluationDomain<ScalarField>, challenges: &Challenges) -> ScalarField {
-    domain.evaluate_vanishing_polynomial(challenges.zeta)
+    let Challenges { zeta, .. } = challenges;
+
+    domain.evaluate_vanishing_polynomial(*zeta)
 }
 
 /// Compute first Lagrange polynomial evaluation at challenge point `zeta`
@@ -116,7 +150,12 @@ fn step_6(
     domain: &Radix2EvaluationDomain<ScalarField>,
     challenges: &Challenges,
 ) -> ScalarField {
-    zero_poly_eval_div_n * domain.group_gen / (challenges.zeta - domain.group_gen)
+    let Radix2EvaluationDomain {
+        group_gen: omega, ..
+    } = domain;
+    let Challenges { zeta, .. } = challenges;
+
+    zero_poly_eval_div_n * *omega / (*zeta - *omega)
 }
 
 /// Evaluate public inputs polynomial at challenge point `zeta`
@@ -127,11 +166,13 @@ fn step_7(
     challenges: &Challenges,
     public_inputs: &[ScalarField],
 ) -> ScalarField {
+    let Challenges { zeta, .. } = challenges;
+
     // TODO: Can factor out constant term `zero_poly_eval_div_n` from sum across Lagrange bases
     let mut pi_eval = lagrange_1_eval * public_inputs[0];
     for (i, pi) in public_inputs.iter().enumerate().skip(1) {
         let omega_i = domain.element(i);
-        let lagrange_i_eval = zero_poly_eval_div_n * omega_i / (challenges.zeta - omega_i);
+        let lagrange_i_eval = zero_poly_eval_div_n * omega_i / (*zeta - omega_i);
         pi_eval += lagrange_i_eval * pi;
     }
     pi_eval
@@ -144,155 +185,220 @@ fn step_8(
     challenges: &Challenges,
     proof: &Proof,
 ) -> ScalarField {
-    let mut r_0 = pi_eval - lagrange_1_eval * challenges.alpha * challenges.alpha;
-    let first_wire_evals = &proof.wire_evals[0..NUM_WIRE_TYPES - 1];
-    let last_wire_eval = &proof.wire_evals[NUM_WIRE_TYPES - 1];
+    let Challenges {
+        alpha, beta, gamma, ..
+    } = challenges;
+    let Proof {
+        a_bar,
+        b_bar,
+        c_bar,
+        sigma_1_bar,
+        sigma_2_bar,
+        z_bar,
+        ..
+    } = proof;
 
-    r_0 -= first_wire_evals
-        .iter()
-        .zip(proof.permutation_evals.iter())
-        .fold(
-            challenges.alpha * (challenges.gamma + last_wire_eval) * proof.grand_product_eval,
-            |acc, (wire_eval, permutation_eval)| {
-                acc * (wire_eval + &(challenges.beta * permutation_eval) + challenges.gamma)
-            },
-        );
-
-    r_0
+    pi_eval
+        - lagrange_1_eval * *alpha * *alpha
+        - *alpha
+            * (*a_bar + *beta * *sigma_1_bar + *gamma)
+            * (*b_bar + *beta * *sigma_2_bar + *gamma)
+            * (*c_bar + *gamma)
+            * *z_bar
 }
 
 /// Compute first part of batched polynomial commitment [D]1
-fn step_9<G: G1EcArithmetic>(
+fn step_9<G: G1ArithmeticBackend>(
     zero_poly_eval: ScalarField,
     lagrange_1_eval: ScalarField,
     vkey: &VerificationKey,
     proof: &Proof,
     challenges: &Challenges,
 ) -> Result<G1Affine, VerifierError> {
-    // Step 9 line 1: MSM over selector polynomial commitments
-    // We suffer some loss of generality here by picking wire polynomial evaluations,
-    // and their corresponding selector polynomial commitments, by hand.
-    let line_1_scalars = [
-        proof.wire_evals[0] * proof.wire_evals[1],
-        proof.wire_evals[0],
-        proof.wire_evals[1],
-        proof.wire_evals[2],
-    ];
-    let line_1_points = [
-        vkey.selector_comms[0],
-        vkey.selector_comms[1],
-        vkey.selector_comms[2],
-        vkey.selector_comms[3],
-    ];
-    let line_1_result = line_1_scalars
-        .iter()
-        .zip(line_1_points.iter())
-        .try_fold(vkey.selector_comms[4], |acc, (scalar, point)| {
-            G::ec_add(acc, G::ec_scalar_mul(*scalar, *point)?)
-        })?;
-
-    // Step 9 line 2: Scalar mul of grand product polynomial commitment
-    let line_2_result = G::ec_scalar_mul(
-        lagrange_1_eval * challenges.alpha * challenges.alpha
-            + challenges.u
-            + (proof.wire_evals.iter().zip(vkey.k.iter()).fold(
-                challenges.alpha,
-                |acc, (wire_eval, k)| {
-                    acc * (wire_eval + &(challenges.beta * k * challenges.zeta) + challenges.gamma)
-                },
-            )),
-        proof.grand_product_comm,
-    )?;
-
-    // Step 9 line 3: Scalar mul of final permutation polynomial commitment
-    let line_3_result = G::ec_scalar_mul(
-        proof
-            .wire_evals
-            .iter()
-            .take(NUM_WIRE_TYPES - 1)
-            .zip(proof.permutation_evals.iter())
-            .fold(
-                -(challenges.alpha * challenges.beta * proof.grand_product_eval),
-                |acc, (wire_eval, permutation_eval)| {
-                    acc * (wire_eval + &(challenges.beta * permutation_eval) + challenges.gamma)
-                },
-            ),
-        vkey.permutation_comms[NUM_WIRE_TYPES - 1],
-    )?;
-
-    // Step 9 line 4: MSM over split quotient polynomial commitments
-    let zeta_to_n = zero_poly_eval + ScalarField::one();
-    let mut line_4_scalar = zeta_to_n;
-    let mut line_4_result = proof.split_quotient_comms.iter().skip(1).try_fold(
-        proof.split_quotient_comms[0],
-        |acc, point| {
-            let result = G::ec_add(acc, G::ec_scalar_mul(line_4_scalar, *point)?);
-            line_4_scalar *= zeta_to_n;
-            result
-        },
-    )?;
-    line_4_result = G::ec_scalar_mul(-zero_poly_eval, line_4_result)?;
-
-    // TODO: Using `fold` here induces an extra EC op due to the initial identity point, can optimize this
-    let d_1 = [line_1_result, line_2_result, line_3_result, line_4_result]
-        .iter()
-        .try_fold(G1Affine::identity(), |acc, point| G::ec_add(acc, *point))?;
-
-    Ok(d_1)
+    G::msm(
+        &[ScalarField::one(); 4],
+        &[
+            step_9_line_1::<G>(vkey, proof)?,
+            step_9_line_2::<G>(lagrange_1_eval, vkey, proof, challenges)?,
+            step_9_line_3::<G>(vkey, proof, challenges)?,
+            step_9_line_4::<G>(zero_poly_eval, proof)?,
+        ],
+    )
 }
 
-/// Compute full batched polynomial commitment [F]1
-fn step_10<G: G1EcArithmetic>(
-    d_1: G1Affine,
+/// MSM over selector polynomial commitments
+fn step_9_line_1<G: G1ArithmeticBackend>(
+    vkey: &VerificationKey,
+    proof: &Proof,
+) -> Result<G1Affine, VerifierError> {
+    let VerificationKey {
+        q_m_comm,
+        q_l_comm,
+        q_r_comm,
+        q_o_comm,
+        q_c_comm,
+        ..
+    } = vkey;
+    let Proof {
+        a_bar,
+        b_bar,
+        c_bar,
+        ..
+    } = proof;
+
+    G::msm(
+        &[*a_bar * *b_bar, *a_bar, *b_bar, *c_bar, ScalarField::one()],
+        &[*q_m_comm, *q_l_comm, *q_r_comm, *q_o_comm, *q_c_comm],
+    )
+}
+
+/// Scalar mul of grand product polynomial commitment
+fn step_9_line_2<G: G1ArithmeticBackend>(
+    lagrange_1_eval: ScalarField,
     vkey: &VerificationKey,
     proof: &Proof,
     challenges: &Challenges,
 ) -> Result<G1Affine, VerifierError> {
-    let mut step_10_scalar = challenges.v;
-    let f_1 = proof
-        .wire_comms
-        .iter()
-        .chain(vkey.permutation_comms.iter().take(NUM_WIRE_TYPES - 1))
-        .try_fold(d_1, |acc, point| {
-            let result = G::ec_add(acc, G::ec_scalar_mul(step_10_scalar, *point)?);
-            step_10_scalar *= challenges.v;
-            result
-        })?;
+    let VerificationKey { k1, k2, .. } = vkey;
+    let Proof {
+        a_bar,
+        b_bar,
+        c_bar,
+        z_comm,
+        ..
+    } = proof;
+    let Challenges {
+        alpha,
+        beta,
+        gamma,
+        zeta,
+        u,
+        ..
+    } = challenges;
 
-    Ok(f_1)
+    G::ec_scalar_mul(
+        (*a_bar + *beta * *zeta + *gamma)
+            * (*b_bar + *beta * *k1 * *zeta + *gamma)
+            * (*c_bar + *beta * *k2 * *zeta + *gamma)
+            * *alpha
+            + lagrange_1_eval * *alpha * *alpha
+            + *u,
+        *z_comm,
+    )
+}
+
+/// Scalar mul of final permutation polynomial commitment
+fn step_9_line_3<G: G1ArithmeticBackend>(
+    vkey: &VerificationKey,
+    proof: &Proof,
+    challenges: &Challenges,
+) -> Result<G1Affine, VerifierError> {
+    let VerificationKey { sigma_3_comm, .. } = vkey;
+    let Proof {
+        a_bar,
+        b_bar,
+        sigma_1_bar,
+        sigma_2_bar,
+        z_bar,
+        ..
+    } = proof;
+    let Challenges {
+        alpha, beta, gamma, ..
+    } = challenges;
+
+    G::ec_scalar_mul(
+        -(*a_bar + *beta * *sigma_1_bar + *gamma)
+            * (*b_bar + *beta * *sigma_2_bar + *gamma)
+            * *alpha
+            * *beta
+            * *z_bar,
+        *sigma_3_comm,
+    )
+}
+
+/// MSM over split quotient polynomial commitments
+fn step_9_line_4<G: G1ArithmeticBackend>(
+    zero_poly_eval: ScalarField,
+    proof: &Proof,
+) -> Result<G1Affine, VerifierError> {
+    let Proof {
+        t_lo_comm,
+        t_mid_comm,
+        t_hi_comm,
+        ..
+    } = proof;
+
+    let zeta_to_n = zero_poly_eval + ScalarField::one();
+    G::ec_scalar_mul(
+        -zero_poly_eval,
+        G::msm(
+            &[ScalarField::one(), zeta_to_n, zeta_to_n * zeta_to_n],
+            &[*t_lo_comm, *t_mid_comm, *t_hi_comm],
+        )?,
+    )
+}
+
+/// Compute full batched polynomial commitment [F]1
+fn step_10<G: G1ArithmeticBackend>(
+    d_1: G1Affine,
+    v_powers: &[ScalarField; 6],
+    vkey: &VerificationKey,
+    proof: &Proof,
+) -> Result<G1Affine, VerifierError> {
+    let VerificationKey {
+        sigma_1_comm,
+        sigma_2_comm,
+        ..
+    } = vkey;
+    let Proof {
+        a_comm,
+        b_comm,
+        c_comm,
+        ..
+    } = proof;
+
+    G::msm(
+        v_powers,
+        &[d_1, *a_comm, *b_comm, *c_comm, *sigma_1_comm, *sigma_2_comm],
+    )
 }
 
 /// Compute group-encoded batch evaluation [E]1
 ///
 /// We negate the scalar here to obtain -[E]1 so that we can avoid another EC scalar mul in step 12
-fn step_11<G: G1EcArithmetic>(
+fn step_11<G: G1ArithmeticBackend>(
     r_0: ScalarField,
+    v_powers: &[ScalarField; 6],
     vkey: &VerificationKey,
-    challenges: &Challenges,
     proof: &Proof,
+    challenges: &Challenges,
 ) -> Result<G1Affine, VerifierError> {
-    let mut step_11_scalar = challenges.v;
-    let neg_e_1 = G::ec_scalar_mul(
-        -proof
-            .wire_evals
-            .iter()
-            .chain(proof.permutation_evals.iter().take(NUM_WIRE_TYPES - 1))
-            .fold(
-                -r_0 + challenges.u * proof.grand_product_eval,
-                |acc, eval| {
-                    let result = acc + step_11_scalar * eval;
-                    step_11_scalar *= challenges.v;
-                    result
-                },
-            ),
-        vkey.g,
-    )?;
+    let VerificationKey { g, .. } = vkey;
+    let Proof {
+        a_bar,
+        b_bar,
+        c_bar,
+        sigma_1_bar,
+        sigma_2_bar,
+        z_bar,
+        ..
+    } = proof;
+    let Challenges { u, .. } = challenges;
 
-    Ok(neg_e_1)
+    G::ec_scalar_mul(
+        -(-r_0
+            + v_powers[1] * *a_bar
+            + v_powers[2] * *b_bar
+            + v_powers[3] * *c_bar
+            + v_powers[4] * *sigma_1_bar
+            + v_powers[5] * *sigma_2_bar
+            + *u * *z_bar),
+        *g,
+    )
 }
 
 /// Batch validate all evaluations
-fn step_12<G: G1EcArithmetic>(
+fn step_12<G: G1ArithmeticBackend>(
     f_1: G1Affine,
     neg_e_1: G1Affine,
     domain: &Radix2EvaluationDomain<ScalarField>,
@@ -300,22 +406,29 @@ fn step_12<G: G1EcArithmetic>(
     proof: &Proof,
     challenges: &Challenges,
 ) -> Result<bool, VerifierError> {
-    let pairing_1_g1_point = G::ec_add(
-        proof.opening_proof,
-        G::ec_scalar_mul(challenges.u, proof.shifted_opening_proof)?,
-    )?;
+    let Radix2EvaluationDomain {
+        group_gen: omega, ..
+    } = domain;
+    let VerificationKey { h, x_h, .. } = vkey;
+    let Proof {
+        w_zeta,
+        w_zeta_omega,
+        ..
+    } = proof;
+    let Challenges { zeta, u, .. } = challenges;
 
-    let pairing_2_g1_scalars = [
-        challenges.zeta,
-        challenges.u * challenges.zeta * domain.group_gen,
-    ];
-    let pairing_2_g1_points = [proof.opening_proof, proof.shifted_opening_proof];
-    let pairing_2_g1_point = pairing_2_g1_scalars
-        .iter()
-        .zip(pairing_2_g1_points.iter())
-        .try_fold(G::ec_add(f_1, neg_e_1)?, |acc, (scalar, point)| {
-            G::ec_add(acc, G::ec_scalar_mul(*scalar, *point)?)
-        })?;
-
-    G::ec_pairing_check(pairing_1_g1_point, vkey.x_h, pairing_2_g1_point, vkey.h)
+    G::ec_pairing_check(
+        G::msm(&[ScalarField::one(), *u], &[*w_zeta, *w_zeta_omega])?,
+        *x_h,
+        G::msm(
+            &[
+                *zeta,
+                *u * *zeta * *omega,
+                ScalarField::one(),
+                ScalarField::one(),
+            ],
+            &[*w_zeta, *w_zeta_omega, f_1, neg_e_1],
+        )?,
+        *h,
+    )
 }
