99bf1598ae2849fdec58d1966daab3fddda0dd30
Andrew Kirillov <andrew@kirillov.org>
2023-10-16 14:52:03 -0700

    contracts-stylus: verifier: store serialized VerificationKey

--- contracts-stylus/src/verifier.rs
+++ contracts-stylus/src/verifier.rs
@@ -1,14 +1,14 @@
 //! The verifier smart contract, responsible for verifying Plonk proofs.
 
-use stylus_sdk::prelude::*;
+use stylus_sdk::{prelude::*, storage::StorageBytes};
 
 use crate::types::solidity_types::StorageVerificationKey;
 
 #[solidity_storage]
 #[entrypoint]
 struct Verifier {
-    /// The verification key for the circuit
-    vkey: StorageVerificationKey,
+    /// The serialized verification key for the circuit
+    vkey: StorageBytes,
 }
 
 #[external]
