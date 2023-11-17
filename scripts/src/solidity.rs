4a8100e76308fc8423f57a5f49324779ded69cda
Andrew Kirillov <andrew@kirillov.org>
2023-11-17 10:38:57 -0800

    scripts: add upgrade command

--- scripts/src/solidity.rs
+++ scripts/src/solidity.rs
@@ -1,7 +1,15 @@
 //! Definitions of Solidity functions called during deployment
 
 use alloy_sol_types::sol;
+use ethers::contract::abigen;
 
 sol! {
     function initialize(address memory owner, address memory verifier_address, address memory merkle_address) external;
 }
+
+abigen!(
+    ProxyAdminContract,
+    r#"[
+        function upgradeAndCall(address proxy, address implementation, bytes memory data) external;
+    ]"#,
+);
