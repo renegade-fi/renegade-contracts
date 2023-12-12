4681fd6943ea95cfbc6324af92338c2ef6949236
Andrew Kirillov <andrew@kirillov.org>
2023-12-12 13:35:37 -0800

    contracts-stylus: merkle: zero out next_index in init method

--- contracts-stylus/src/contracts/test_contracts/merkle_test_contract.rs
+++ contracts-stylus/src/contracts/test_contracts/merkle_test_contract.rs
@@ -2,10 +2,7 @@
 
 use alloc::vec::Vec;
 use common::constants::TEST_MERKLE_HEIGHT;
-use stylus_sdk::{
-    alloy_primitives::{U128, U256},
-    prelude::*,
-};
+use stylus_sdk::{alloy_primitives::U256, prelude::*};
 
 use crate::contracts::merkle::{MerkleContract, MerkleParams};
 
@@ -38,15 +35,5 @@ impl TestMerkleContract {
 
     fn insert_shares_commitment(&mut self, shares: Vec<U256>) -> Result<(), Vec<u8>> {
         self.merkle.insert_shares_commitment(shares)
-    }
-
-    fn clear(&mut self) -> Result<(), Vec<u8>> {
-        self.merkle.init()?;
-        self.merkle.next_index.set(U128::ZERO);
-
-        // No straightforward way to clear the `root_history` map,
-        // so we just leave it as is.
-
-        Ok(())
     }
 }
