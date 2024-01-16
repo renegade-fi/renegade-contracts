5c955f0a6638d5bef8d064407e02e5cdf56da144
Andrew Kirillov <andrew@kirillov.org>
2024-01-16 11:23:02 -0800

    contracts-stylus: remove all unwraps

--- contracts-stylus/src/utils/helpers.rs
+++ contracts-stylus/src/utils/helpers.rs
@@ -7,18 +7,32 @@ use contracts_common::{
     constants::NUM_SCALARS_PK,
     custom_serde::{
         bigint_from_le_bytes, pk_to_scalars, statement_to_public_inputs, BytesSerializable,
-        ScalarSerializable, SerdeError,
+        ScalarSerializable,
     },
     types::{
         MatchPublicInputs, PublicSigningKey, ScalarField, ValidCommitmentsStatement,
         ValidMatchSettleStatement, ValidReblindStatement,
     },
 };
+use serde::Deserialize;
 use stylus_sdk::{
+    abi::Bytes,
     alloy_primitives::{Address, U256},
     call::{delegate_call, static_call},
     storage::TopLevelStorage,
 };
+
+use super::constants::{
+    CALLDATA_DESER_ERROR_MESSAGE, CALLDATA_SER_ERROR_MESSAGE, CALL_RETDATA_DECODING_ERROR_MESSAGE,
+    INVALID_ARR_LEN_ERROR_MESSAGE, SCALAR_CONVERSION_ERROR_MESSAGE,
+};
+
+/// Deserializes a byte-serialized type from calldata
+pub fn deserialize_from_calldata<'a, D: Deserialize<'a>>(
+    calldata: &'a Bytes,
+) -> Result<D, Vec<u8>> {
+    postcard::from_bytes(calldata.as_slice()).map_err(|_| CALLDATA_DESER_ERROR_MESSAGE.to_vec())
+}
 
 /// Serializes the given statement into scalars, and then into bytes,
 /// as expected by the verifier contract.
@@ -28,8 +42,9 @@ use stylus_sdk::{
 )]
 pub fn serialize_statement_for_verification<S: ScalarSerializable>(
     statement: &S,
-) -> postcard::Result<Vec<u8>> {
+) -> Result<Vec<u8>, Vec<u8>> {
     postcard::to_allocvec(&statement_to_public_inputs(statement))
+        .map_err(|_| CALLDATA_SER_ERROR_MESSAGE.to_vec())
 }
 
 /// Serializes the statements used in verifying the settlement of a
@@ -45,7 +60,7 @@ pub fn serialize_match_statements_for_verification(
     valid_reblind_0: &ValidReblindStatement,
     valid_reblind_1: &ValidReblindStatement,
     valid_match_settle: &ValidMatchSettleStatement,
-) -> postcard::Result<Vec<u8>> {
+) -> Result<Vec<u8>, Vec<u8>> {
     let match_public_inputs = MatchPublicInputs {
         valid_commitments_0: statement_to_public_inputs(valid_commitments_0),
         valid_commitments_1: statement_to_public_inputs(valid_commitments_1),
@@ -53,7 +68,18 @@ pub fn serialize_match_statements_for_verification(
         valid_reblind_1: statement_to_public_inputs(valid_reblind_1),
         valid_match_settle: statement_to_public_inputs(valid_match_settle),
     };
-    postcard::to_allocvec(&match_public_inputs)
+    postcard::to_allocvec(&match_public_inputs).map_err(|_| CALLDATA_SER_ERROR_MESSAGE.to_vec())
+}
+
+/// Maps an error returned from an external contract call to a `Vec<u8>`,
+/// which is the expected return type of external contract methods.
+pub fn map_call_error(e: stylus_sdk::call::Error) -> Vec<u8> {
+    match e {
+        stylus_sdk::call::Error::Revert(msg) => msg,
+        stylus_sdk::call::Error::AbiDecodingFailed(_) => {
+            CALL_RETDATA_DECODING_ERROR_MESSAGE.to_vec()
+        }
+    }
 }
 
 /// Performs a `delegatecall` to the given address, calling the function
@@ -68,13 +94,9 @@ pub fn delegate_call_helper<C: SolCall>(
     args: <C::Arguments<'_> as SolType>::RustType,
 ) -> Result<C::Return, Vec<u8>> {
     let calldata = C::new(args).encode();
-    let res = unsafe {
-        delegate_call(storage, address, &calldata).map_err(|e| match e {
-            stylus_sdk::call::Error::Revert(msg) => msg,
-            stylus_sdk::call::Error::AbiDecodingFailed(_) => b"abi decoding failed".to_vec(),
-        })?
-    };
-    Ok(C::decode_returns(&res, true /* validate */).unwrap())
+    let res = unsafe { delegate_call(storage, address, &calldata).map_err(map_call_error)? };
+    C::decode_returns(&res, true /* validate */)
+        .map_err(|_| CALL_RETDATA_DECODING_ERROR_MESSAGE.to_vec())
 }
 
 /// Performs a `staticcall` to the given address, calling the function
@@ -87,10 +109,11 @@ pub fn static_call_helper<C: SolCall>(
     storage: &mut impl TopLevelStorage,
     address: Address,
     args: <C::Arguments<'_> as SolType>::RustType,
-) -> C::Return {
+) -> Result<C::Return, Vec<u8>> {
     let calldata = C::new(args).encode();
-    let res = static_call(storage, address, &calldata).unwrap();
-    C::decode_returns(&res, true /* validate */).unwrap()
+    let res = static_call(storage, address, &calldata).map_err(map_call_error)?;
+    C::decode_returns(&res, true /* validate */)
+        .map_err(|_| CALL_RETDATA_DECODING_ERROR_MESSAGE.to_vec())
 }
 
 /// Converts a scalar to a U256
@@ -116,9 +139,10 @@ pub fn scalar_to_u256(scalar: ScalarField) -> U256 {
     )),
     allow(dead_code)
 )]
-pub fn u256_to_scalar(u256: U256) -> Result<ScalarField, SerdeError> {
-    let bigint = bigint_from_le_bytes(&u256.to_le_bytes_vec())?;
-    ScalarField::from_bigint(bigint).ok_or(SerdeError::ScalarConversion)
+pub fn u256_to_scalar(u256: U256) -> Result<ScalarField, Vec<u8>> {
+    let bigint = bigint_from_le_bytes(&u256.to_le_bytes_vec())
+        .map_err(|_| SCALAR_CONVERSION_ERROR_MESSAGE.to_vec())?;
+    ScalarField::from_bigint(bigint).ok_or(SCALAR_CONVERSION_ERROR_MESSAGE.to_vec())
 }
 
 /// Converts a [`PublicSigningKey`] into the [`U256`] array representing its scalar serialization
@@ -126,14 +150,14 @@ pub fn u256_to_scalar(u256: U256) -> Result<ScalarField, SerdeError> {
     not(any(feature = "darkpool", feature = "darkpool-test-contract")),
     allow(dead_code)
 )]
-pub fn pk_to_u256s(pk: &PublicSigningKey) -> [U256; NUM_SCALARS_PK] {
+pub fn pk_to_u256s(pk: &PublicSigningKey) -> Result<[U256; NUM_SCALARS_PK], Vec<u8>> {
     let scalars = pk_to_scalars(pk);
     scalars
         .into_iter()
         .map(scalar_to_u256)
         .collect::<Vec<_>>()
         .try_into()
-        .unwrap()
+        .map_err(|_| INVALID_ARR_LEN_ERROR_MESSAGE.to_vec())
 }
 
 /// Expands to the given code block if the `no-verify` feature is not enabled.
