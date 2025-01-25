48903cd9695b875ab54c5a54c92f29f1bda25675
Joey Kraut <108701651+joeykraut@users.noreply.github.com>
2025-01-25 15:01:21 -0800

    crypto: poseidon2: Add helpers for round constants and internal MDS (#4)
    * crypto: poseidon2: roundUtils: Add round constant helpers
    
    * crypto: poseidon2: Add helpers for internal MDS

--- test/Poseidon.t.sol
+++ test/Poseidon.t.sol
@@ -11,6 +11,8 @@ contract PoseidonTest is Test {
 
     /// @dev The BN254 field modulus from roundUtils.huff
     uint256 PRIME = 0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001;
+    /// @dev The round constant used in testing
+    uint256 TEST_RC = 0x1337;
 
     /// @dev Deploy the PoseidonSuite contract
     function setUp() public {
@@ -28,8 +30,39 @@ contract PoseidonTest is Test {
         uint256 expected = mulmod(testValue, x4, PRIME);
         assertEq(result, expected, "Expected result to match x^5 mod p");
     }
+
+    /// @dev Test the add round constant function applied to a single input
+    function testAddRcSingle() public {
+        uint256 testValue = vm.randomUint();
+        uint256 result = poseidonSuite.testAddRc(testValue);
+        uint256 expected = addmod(testValue, TEST_RC, PRIME);
+        assertEq(result, expected, "Expected result to match x + RC mod p");
+    }
+
+    /// @dev Test the internal MDS function applied to a single input
+    /// The internal MDS adds the sum of the elements to each element
+    function testInternalMds() public {
+        uint256 a = vm.randomUint();
+        uint256 b = vm.randomUint();
+        uint256 c = vm.randomUint();
+        (uint256 a1, uint256 b1, uint256 c1) = poseidonSuite.testInternalMds(a, b, c);
+
+        // Calculate the sum of the elements
+        uint256 sum = addmod(a, b, PRIME);
+        sum = addmod(sum, c, PRIME);
+
+        // Calculate the expected results
+        uint256 expectedA = addmod(a, sum, PRIME);
+        uint256 expectedB = addmod(b, sum, PRIME);
+        uint256 expectedC = addmod(c, sum, PRIME);
+        assertEq(a1, expectedA, "Expected result to match a + sum mod p");
+        assertEq(b1, expectedB, "Expected result to match b + sum mod p");
+        assertEq(c1, expectedC, "Expected result to match c + sum mod p");
+    }
 }
 
 interface PoseidonSuite {
     function testSboxSingle(uint256) external returns (uint256);
+    function testAddRc(uint256) external returns (uint256);
+    function testInternalMds(uint256, uint256, uint256) external returns (uint256, uint256, uint256);
 }
