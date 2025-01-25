148c11d8e13a4eb6d1916047766f2f10cbfb8c29
Joey Kraut <joey@renegade.fi>
2025-01-25 13:02:28 -0800

    crypto: poseidon2: roundUtils: Add round constant helpers

--- test/Poseidon.t.sol
+++ test/Poseidon.t.sol
@@ -11,6 +11,8 @@ contract PoseidonTest is Test {
 
     /// @dev The BN254 field modulus from roundUtils.huff
     uint256 PRIME = 0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001;
+    /// @dev The round constant used in testing
+    uint256 TEST_RC = 0x1337;
 
     /// @dev Deploy the PoseidonSuite contract
     function setUp() public {
@@ -28,8 +30,17 @@ contract PoseidonTest is Test {
         uint256 expected = mulmod(testValue, x4, PRIME);
         assertEq(result, expected, "Expected result to match x^5 mod p");
     }
+
+    /// @dev Test the add round constant function applied to a single input
+    function testAddRcSingle() public {
+        uint256 testValue = vm.randomUint();
+        uint256 result = poseidonSuite.testAddRc(testValue);
+        uint256 expected = addmod(testValue, TEST_RC, PRIME);
+        assertEq(result, expected, "Expected result to match x + RC mod p");
+    }
 }
 
 interface PoseidonSuite {
     function testSboxSingle(uint256) external returns (uint256);
+    function testAddRc(uint256) external returns (uint256);
 }
