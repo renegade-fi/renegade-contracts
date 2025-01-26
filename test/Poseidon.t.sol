08b3ffb905f28aac9cf25aebece02b41b8b67e0d
Joey Kraut <joey@renegade.fi>
2025-01-25 17:36:56 -0800

    crypto: poseidon2: Add external round macro impl

--- test/Poseidon.t.sol
+++ test/Poseidon.t.sol
@@ -12,7 +12,11 @@ contract PoseidonTest is Test {
     /// @dev The BN254 field modulus from roundUtils.huff
     uint256 PRIME = 0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001;
     /// @dev The round constant used in testing
-    uint256 TEST_RC = 0x1337;
+    uint256 TEST_RC1 = 0x1337;
+    /// @dev The second round constant used in testing
+    uint256 TEST_RC2 = 0x1338;
+    /// @dev The third round constant used in testing
+    uint256 TEST_RC3 = 0x1339;
 
     /// @dev Deploy the PoseidonSuite contract
     function setUp() public {
@@ -25,9 +29,7 @@ contract PoseidonTest is Test {
         uint256 result = poseidonSuite.testSboxSingle(testValue);
 
         // Calculate expected x^5 mod p
-        uint256 x2 = mulmod(testValue, testValue, PRIME);
-        uint256 x4 = mulmod(x2, x2, PRIME);
-        uint256 expected = mulmod(testValue, x4, PRIME);
+        uint256 expected = fifthPower(testValue);
         assertEq(result, expected, "Expected result to match x^5 mod p");
     }
 
@@ -35,7 +37,7 @@ contract PoseidonTest is Test {
     function testAddRcSingle() public {
         uint256 testValue = vm.randomUint();
         uint256 result = poseidonSuite.testAddRc(testValue);
-        uint256 expected = addmod(testValue, TEST_RC, PRIME);
+        uint256 expected = addmod(testValue, TEST_RC1, PRIME);
         assertEq(result, expected, "Expected result to match x + RC mod p");
     }
 
@@ -47,14 +49,8 @@ contract PoseidonTest is Test {
         uint256 c = vm.randomUint();
         (uint256 a1, uint256 b1, uint256 c1) = poseidonSuite.testInternalMds(a, b, c);
 
-        // Calculate the sum of the elements
-        uint256 sum = addmod(a, b, PRIME);
-        sum = addmod(sum, c, PRIME);
-
         // Calculate the expected results
-        uint256 expectedA = addmod(a, sum, PRIME);
-        uint256 expectedB = addmod(b, sum, PRIME);
-        uint256 expectedC = addmod(c, addmod(c, sum, PRIME), PRIME);
+        (uint256 expectedA, uint256 expectedB, uint256 expectedC) = internalMds(a, b, c);
         assertEq(a1, expectedA, "Expected result to match a + sum mod p");
         assertEq(b1, expectedB, "Expected result to match b + sum mod p");
         assertEq(c1, expectedC, "Expected result to match c + sum mod p");
@@ -67,17 +63,45 @@ contract PoseidonTest is Test {
         uint256 c = vm.randomUint();
         (uint256 a1, uint256 b1, uint256 c1) = poseidonSuite.testExternalMds(a, b, c);
 
-        // Calculate the sum of the elements
-        uint256 sum = addmod(a, b, PRIME);
-        sum = addmod(sum, c, PRIME);
-
         // Calculate the expected results
-        uint256 expectedA = addmod(a, sum, PRIME);
-        uint256 expectedB = addmod(b, sum, PRIME);
-        uint256 expectedC = addmod(c, sum, PRIME);
+        (uint256 expectedA, uint256 expectedB, uint256 expectedC) = externalMds(a, b, c);
         assertEq(a1, expectedA, "Expected result to match a + sum mod p");
         assertEq(b1, expectedB, "Expected result to match b + sum mod p");
         assertEq(c1, expectedC, "Expected result to match c + sum mod p");
+    }
+
+    /// --- Helpers --- ///
+
+    /// @dev Calculate the fifth power of an input
+    function fifthPower(uint256 x) internal view returns (uint256) {
+        uint256 x2 = mulmod(x, x, PRIME);
+        uint256 x4 = mulmod(x2, x2, PRIME);
+        return mulmod(x, x4, PRIME);
+    }
+
+    /// @dev Calculate the result of the internal MDS matrix applied to the inputs
+    function internalMds(uint256 a, uint256 b, uint256 c) internal view returns (uint256, uint256, uint256) {
+        uint256 sum = sumInputs(a, b, c);
+        uint256 a1 = addmod(a, sum, PRIME);
+        uint256 b1 = addmod(b, sum, PRIME);
+        uint256 c1 = addmod(addmod(c, sum, PRIME), c, PRIME); // c is doubled
+        return (a1, b1, c1);
+    }
+
+    /// @dev Calculate the result of the external MDS matrix applied to the inputs
+    function externalMds(uint256 a, uint256 b, uint256 c) internal view returns (uint256, uint256, uint256) {
+        uint256 sum = sumInputs(a, b, c);
+        uint256 a1 = addmod(a, sum, PRIME);
+        uint256 b1 = addmod(b, sum, PRIME);
+        uint256 c1 = addmod(c, sum, PRIME);
+        return (a1, b1, c1);
+    }
+
+    /// @dev Sum the inputs and return the result
+    function sumInputs(uint256 a, uint256 b, uint256 c) internal view returns (uint256) {
+        uint256 sum = addmod(a, b, PRIME);
+        sum = addmod(sum, c, PRIME);
+        return sum;
     }
 }
 
@@ -86,4 +110,5 @@ interface PoseidonSuite {
     function testAddRc(uint256) external returns (uint256);
     function testInternalMds(uint256, uint256, uint256) external returns (uint256, uint256, uint256);
     function testExternalMds(uint256, uint256, uint256) external returns (uint256, uint256, uint256);
+    function testExternalRound(uint256, uint256, uint256) external returns (uint256, uint256, uint256);
 }
