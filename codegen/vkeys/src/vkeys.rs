0c9e2cf906e7149915f8d357104cf1930c7639bb
Joey Kraut <joey@renegade.fi>
2025-03-07 08:50:02 -0800

    codegen: vkeys: Add linking vkeys to codegen script

--- codegen/vkeys/src/vkeys.rs
+++ codegen/vkeys/src/vkeys.rs
@@ -2,16 +2,23 @@
 
 use std::fmt::{self, Display};
 
-use reference_impl_common::abi_types::VerificationKey;
+use reference_impl_common::abi_types::{ProofLinkingVK, VerificationKey};
 use renegade_circuit_types::traits::SingleProverCircuit;
 use renegade_circuits::zk_circuits::{
-    valid_commitments::SizedValidCommitments, valid_fee_redemption::SizedValidFeeRedemption,
+    proof_linking::{
+        get_commitments_match_settle_group_layout, get_reblind_commitments_group_layout,
+    },
+    valid_commitments::SizedValidCommitments,
+    valid_fee_redemption::SizedValidFeeRedemption,
     valid_malleable_match_settle_atomic::SizedValidMalleableMatchSettleAtomic,
     valid_match_settle::SizedValidMatchSettle,
     valid_match_settle_atomic::SizedValidMatchSettleAtomic,
-    valid_offline_fee_settlement::SizedValidOfflineFeeSettlement, valid_reblind::SizedValidReblind,
-    valid_wallet_create::SizedValidWalletCreate, valid_wallet_update::SizedValidWalletUpdate,
+    valid_offline_fee_settlement::SizedValidOfflineFeeSettlement,
+    valid_reblind::SizedValidReblind,
+    valid_wallet_create::SizedValidWalletCreate,
+    valid_wallet_update::SizedValidWalletUpdate,
 };
+use renegade_constants::{MAX_BALANCES, MAX_ORDERS, MERKLE_HEIGHT};
 
 /// The circuit to generate a verification key for
 #[derive(Debug, Clone, Copy)]
@@ -98,6 +105,52 @@ impl Circuit {
     }
 }
 
+/// Represents all the linking instances in the Renegade circuits
+#[derive(Debug, Clone, Copy)]
+pub enum LinkingInstance {
+    /// The proof link between `VALID REBLIND` and `VALID COMMITMENTS`
+    ValidReblindCommitments,
+    /// The proof link between `VALID COMMITMENTS` and `VALID MATCH SETTLE` for the first party
+    ValidCommitmentsMatchSettle0,
+    /// The proof link between `VALID COMMITMENTS` and `VALID MATCH SETTLE` for the second party
+    ValidCommitmentsMatchSettle1,
+}
+
+impl LinkingInstance {
+    /// Generate a verification key for the linking instance
+    pub fn vkey(&self) -> ProofLinkingVK {
+        match self {
+            Self::ValidReblindCommitments => generate_reblind_commitments_link_vkey(),
+            Self::ValidCommitmentsMatchSettle0 => {
+                generate_commitments_match_settle_link_vkey(0 /* party */)
+            }
+            Self::ValidCommitmentsMatchSettle1 => {
+                generate_commitments_match_settle_link_vkey(1 /* party */)
+            }
+        }
+    }
+
+    /// Get the name of the linking instance
+    pub fn name(&self) -> &'static str {
+        match self {
+            Self::ValidReblindCommitments => "VALID_REBLIND_COMMITMENTS_LINK",
+            Self::ValidCommitmentsMatchSettle0 => "VALID_COMMITMENTS_MATCH_SETTLE_LINK0",
+            Self::ValidCommitmentsMatchSettle1 => "VALID_COMMITMENTS_MATCH_SETTLE_LINK1",
+        }
+    }
+
+    /// Get all linking instances
+    pub fn all() -> Vec<Self> {
+        vec![
+            Self::ValidReblindCommitments,
+            Self::ValidCommitmentsMatchSettle0,
+            Self::ValidCommitmentsMatchSettle1,
+        ]
+    }
+}
+
+// --- Helpers --- //
+
 /// Generate the verification keys for all circuits
 ///
 /// Returns a map from the circuit name to the verification key
@@ -105,4 +158,18 @@ fn generate_vkey_for_circuit<T: SingleProverCircuit>() -> VerificationKey {
     let vk = T::verifying_key();
     let vkey = VerificationKey::from(vk.as_ref().clone());
     vkey
+}
+
+/// Generate the linking verification key for the `VALID REBLIND <-> VALID COMMITMENTS` link
+fn generate_reblind_commitments_link_vkey() -> ProofLinkingVK {
+    let group_layout =
+        get_reblind_commitments_group_layout::<MAX_BALANCES, MAX_ORDERS, MERKLE_HEIGHT>().unwrap();
+    ProofLinkingVK::from(group_layout)
+}
+
+/// Generate the linking verification key for the `VALID COMMITMENTS <-> VALID MATCH SETTLE` link
+fn generate_commitments_match_settle_link_vkey(party: u64) -> ProofLinkingVK {
+    let group_layout =
+        get_commitments_match_settle_group_layout::<MAX_BALANCES, MAX_ORDERS>(party).unwrap();
+    ProofLinkingVK::from(group_layout)
 }
