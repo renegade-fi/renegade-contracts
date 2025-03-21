930995611f6f9dae6976d07000158d786584bd7c
Joey Kraut <joey@renegade.fi>
2025-03-20 18:05:52 -0700

    integration: test: update-wallet: Add basic place/cancel order tests

--- integration/src/util.rs
+++ integration/src/util.rs
@@ -127,3 +127,27 @@ pub async fn call_helper<C: CallDecoder + Unpin>(
     let res = call.call().await?;
     Ok(res)
 }
+
+// ----------
+// | Errors |
+// ----------
+
+/// A trait with auto-implementation that makes it easier to convert errors to `eyre::Result`
+pub trait WrapEyre {
+    /// The type of the value being wrapped
+    type Value;
+
+    /// Convert the error to an eyre::Result
+    fn to_eyre(self) -> Result<Self::Value>;
+}
+
+impl<R, E: ToString> WrapEyre for core::result::Result<R, E> {
+    type Value = R;
+
+    fn to_eyre(self) -> Result<R> {
+        match self {
+            Ok(r) => Ok(r),
+            Err(e) => Err(eyre::eyre!(e.to_string())),
+        }
+    }
+}
