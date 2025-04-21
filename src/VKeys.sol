6c5ab7431f68f1ceede61f609b2295b3784ca27f
Joey Kraut <108701651+joeykraut@users.noreply.github.com>
2025-04-20 23:00:46 -0700

    codegen: vkeys: Generate new vkeys for match circuits with commitments (#99)

--- src/VKeys.sol
+++ src/VKeys.sol
@@ -45,6 +45,31 @@ contract VKeys is IVKeys {
         commitmentsMatchSettleVk1 = deserializeLinkKey(VerificationKeys.VALID_COMMITMENTS_MATCH_SETTLE_LINK1_VKEY);
     }
 
+    /// @notice Match bundle with commitments vkeys
+    ///
+    /// @dev We use the linking vkeys for the match bundle circuit as the `VALID MATCH SETTLE` variants all use
+    /// @dev the same link group layouts
+    function matchBundleWithCommitmentsKeys()
+        external
+        pure
+        override
+        returns (
+            VerificationKey memory commitmentsVk,
+            VerificationKey memory reblindVk,
+            VerificationKey memory settleVk,
+            ProofLinkingVK memory reblindCommitmentsVk,
+            ProofLinkingVK memory commitmentsMatchSettleVk0,
+            ProofLinkingVK memory commitmentsMatchSettleVk1
+        )
+    {
+        commitmentsVk = deserializeKey(VerificationKeys.VALID_COMMITMENTS_VKEY);
+        reblindVk = deserializeKey(VerificationKeys.VALID_REBLIND_VKEY);
+        settleVk = deserializeKey(VerificationKeys.VALID_MATCH_SETTLE_WITH_COMMITMENTS_VKEY);
+        reblindCommitmentsVk = deserializeLinkKey(VerificationKeys.VALID_REBLIND_COMMITMENTS_LINK_VKEY);
+        commitmentsMatchSettleVk0 = deserializeLinkKey(VerificationKeys.VALID_COMMITMENTS_MATCH_SETTLE_LINK0_VKEY);
+        commitmentsMatchSettleVk1 = deserializeLinkKey(VerificationKeys.VALID_COMMITMENTS_MATCH_SETTLE_LINK1_VKEY);
+    }
+
     // Atomic match bundle keys
     function atomicMatchBundleKeys()
         external
@@ -61,6 +86,29 @@ contract VKeys is IVKeys {
         commitmentsVk = deserializeKey(VerificationKeys.VALID_COMMITMENTS_VKEY);
         reblindVk = deserializeKey(VerificationKeys.VALID_REBLIND_VKEY);
         settleVk = deserializeKey(VerificationKeys.VALID_MATCH_SETTLE_ATOMIC_VKEY);
+        reblindCommitmentsVk = deserializeLinkKey(VerificationKeys.VALID_REBLIND_COMMITMENTS_LINK_VKEY);
+        commitmentsMatchSettleVk = deserializeLinkKey(VerificationKeys.VALID_COMMITMENTS_MATCH_SETTLE_LINK0_VKEY);
+    }
+
+    /// @notice Atomic match bundle with commitments vkeys
+    ///
+    /// @dev We use the linking vkeys for the atomic match bundle circuit as the `VALID MATCH SETTLE ATOMIC` variants
+    /// @dev all use the same link group layouts
+    function atomicMatchBundleWithCommitmentsKeys()
+        external
+        pure
+        override
+        returns (
+            VerificationKey memory commitmentsVk,
+            VerificationKey memory reblindVk,
+            VerificationKey memory settleVk,
+            ProofLinkingVK memory reblindCommitmentsVk,
+            ProofLinkingVK memory commitmentsMatchSettleVk
+        )
+    {
+        commitmentsVk = deserializeKey(VerificationKeys.VALID_COMMITMENTS_VKEY);
+        reblindVk = deserializeKey(VerificationKeys.VALID_REBLIND_VKEY);
+        settleVk = deserializeKey(VerificationKeys.VALID_MATCH_SETTLE_ATOMIC_WITH_COMMITMENTS_VKEY);
         reblindCommitmentsVk = deserializeLinkKey(VerificationKeys.VALID_REBLIND_COMMITMENTS_LINK_VKEY);
         commitmentsMatchSettleVk = deserializeLinkKey(VerificationKeys.VALID_COMMITMENTS_MATCH_SETTLE_LINK0_VKEY);
     }
