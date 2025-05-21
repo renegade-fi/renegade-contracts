69d98c6f07a50b10a32f27fa5601e57567864d8f
Joey Kraut <joey@renegade.fi>
2025-05-21 10:22:26 -0700

    contracts-common: types: match: Validate quote amount in bounded match

--- contracts-common/src/types/match.rs
+++ contracts-common/src/types/match.rs
@@ -10,6 +10,8 @@ use super::FixedPoint;
 
 /// The revert message when an invalid base amount is provided
 pub const ERROR_INVALID_BASE_AMT: &[u8] = b"invalid base amount";
+/// The revert message when an invalid quote amount is provided
+pub const ERROR_INVALID_QUOTE_AMT: &[u8] = b"invalid quote amount";
 
 /// The result of an external match
 /// The result of an atomic match
@@ -97,19 +99,10 @@ impl BoundedMatchResult {
     /// given a base amount
     pub fn to_external_match_result(
         &self,
+        quote_amount: U256,
         base_amount: U256,
     ) -> Result<ExternalMatchResult, Vec<u8>> {
-        // Validate the match amount
-        let amount_too_low = base_amount < self.min_base_amount;
-        let amount_too_high = base_amount > self.max_base_amount;
-        if amount_too_low || amount_too_high {
-            return Err(ERROR_INVALID_BASE_AMT.into());
-        }
-
-        // Compute the quote amount
-        let price = self.price;
-        let quote_amount = price.unsafe_fixed_point_mul(base_amount);
-
+        self.validate_amounts(quote_amount, base_amount)?;
         Ok(ExternalMatchResult {
             quote_mint: self.quote_mint,
             base_mint: self.base_mint,
@@ -117,5 +110,81 @@ impl BoundedMatchResult {
             base_amount,
             direction: self.direction,
         })
+    }
+
+    // --- Validation --- //
+
+    /// Validate the base and quote amount for a match
+    fn validate_amounts(&self, quote_amount: U256, base_amount: U256) -> Result<(), Vec<u8>> {
+        self.validate_base_amount(base_amount)?;
+        self.validate_quote_amount(quote_amount, base_amount)
+    }
+
+    /// Validate the base amount for a match
+    ///
+    /// This simply validates that the base amount lies in the range constructed
+    /// by the relayer. This range is validated in-circuit to be well
+    /// capitalized.
+    fn validate_base_amount(&self, base_amount: U256) -> Result<(), Vec<u8>> {
+        let amount_too_low = base_amount < self.min_base_amount;
+        let amount_too_high = base_amount > self.max_base_amount;
+        if amount_too_low || amount_too_high {
+            return Err(ERROR_INVALID_BASE_AMT.into());
+        }
+
+        Ok(())
+    }
+
+    /// Validate the quote amount for a match
+    ///
+    /// We allow an external user to specify a quote amount, but we need to
+    /// ensure that they have not given themselves an invalid amount or price
+    /// improvement at the expense of the internal party.
+    ///
+    /// This involves two checks:
+    /// 1. The quote amount must be within the range implied by the base amount
+    ///    range. Let `min_quote = floor(min_base * price)` and `max_quote =
+    ///    floor(max_base * price)`. The quote amount must lie in the range
+    ///    `[min_quote, max_quote]`.
+    /// 2. The quote amount must imply a price that improves upon the reference
+    ///    price in the match result *for the internal party*. Let
+    ///    `reference_quote = floor(base_amount * price)`. Then for an external
+    ///    sell order, we assert `quote_amount <= reference_quote`; i.e. the
+    ///    external party sells at a lower price. For an external buy order, we
+    ///    assert `quote_amount >= reference_quote`; i.e. the external party
+    ///    buys at a higher price.
+    ///
+    /// Note that we can combine these two checks by taking the intersection of
+    /// their respective intervals. For an external party buy order, this is the
+    /// interval:  `[ref_quote, inf) \cap [min_quote, max_quote] =
+    /// [ref_quote, max_quote]`
+    ///
+    /// For an external party sell order, this is the interval:
+    ///  `[0, ref_quote] \cap [min_quote, max_quote] = [min_quote, ref_quote]`
+    ///
+    /// So we check that the quote lies in the intersection interval
+    ///
+    /// SAFETY: All values below are constrained to be within 100 bits, and the
+    /// price is constrained to be within 127 bits, so wraparound is impossible
+    fn validate_quote_amount(&self, quote_amount: U256, base_amount: U256) -> Result<(), Vec<u8>> {
+        // Compute the quote amount bounds
+        let min_quote = self.price.unsafe_fixed_point_mul(self.min_base_amount);
+        let max_quote = self.price.unsafe_fixed_point_mul(self.max_base_amount);
+        let ref_quote = self.price.unsafe_fixed_point_mul(base_amount);
+
+        // Check that the quote amount lies in the intersection interval
+        let (range_min, range_max) = if self.is_external_party_sell() {
+            (min_quote, ref_quote)
+        } else {
+            (ref_quote, max_quote)
+        };
+
+        let quote_too_low = quote_amount < range_min;
+        let quote_too_high = quote_amount > range_max;
+        if quote_too_low || quote_too_high {
+            return Err(ERROR_INVALID_QUOTE_AMT.into());
+        }
+
+        Ok(())
     }
 }
