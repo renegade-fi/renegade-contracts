dbd801de914250e6f6fd4fe4f912d0acf6760169
Joey Kraut <joey@renegade.fi>
2025-10-15 13:49:53 -0700

    darkpoolv2: settlement: Add public obligation compatibility check

--- src/darkpool/v2/libraries/SettlementLib.sol
+++ src/darkpool/v2/libraries/SettlementLib.sol
@@ -1,22 +1,74 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import { SettlementBundle, IntentBundle } from "darkpoolv2-types/Settlement.sol";
+import { SettlementBundle, IntentBundle, ObligationBundle, ObligationType } from "darkpoolv2-types/Settlement.sol";
+import { SettlementObligation } from "darkpoolv2-types/SettlementObligation.sol";
 
 /// @title SettlementLib
 /// @author Renegade Eng
 /// @notice Library for settlement operations
 library SettlementLib {
+    /// @notice Error thrown when the obligation types are not compatible
+    error IncompatibleObligationTypes();
+    /// @notice Error thrown when the obligation tokens are not compatible
+    error IncompatiblePairs();
+    /// @notice Error thrown when the obligation amounts are not compatible
+    error IncompatibleAmounts();
+
+    // --- Obligation Compatibility --- //
+
     /// @notice Check that two settlement obligations are compatible with one another
-    /// @param party0SettlementBundle The settlement bundle for the first party
-    /// @param party1SettlementBundle The settlement bundle for the second party
+    /// @param party0Bundle The obligation bundle for the first party
+    /// @param party1Bundle The obligation bundle for the second party
     function checkObligationCompatibility(
-        SettlementBundle calldata party0SettlementBundle,
-        SettlementBundle calldata party1SettlementBundle
+        ObligationBundle calldata party0Bundle,
+        ObligationBundle calldata party1Bundle
     )
         public
+        pure
     {
-        // TODO: Implement the compatibility check logic
+        // Parties must have the same obligation type; in that both trades must either settle privately or publicly
+        // Regardless of the intent or balance types
+        if (party0Bundle.obligationType != party1Bundle.obligationType) {
+            revert IncompatibleObligationTypes();
+        }
+
+        ObligationType ty = party0Bundle.obligationType;
+        if (ty == ObligationType.PUBLIC) {
+            // Validate a public obligation
+            validatePublicObligationCompatibility(party0Bundle, party1Bundle);
+        } else {
+            revert("Not implemented");
+        }
+    }
+
+    /// @notice Validate compatibility of two public obligations
+    /// @param party0Bundle The settlement bundle for the first party
+    /// @param party1Bundle The settlement bundle for the second party
+    function validatePublicObligationCompatibility(
+        ObligationBundle calldata party0Bundle,
+        ObligationBundle calldata party1Bundle
+    )
+        public
+        pure
+    {
+        // Decode the obligations
+        SettlementObligation memory party0Obligation = abi.decode(party0Bundle.data, (SettlementObligation));
+        SettlementObligation memory party1Obligation = abi.decode(party1Bundle.data, (SettlementObligation));
+
+        // 1. The input and output tokens must correspond to the same pair
+        bool tokenCompatible = party0Obligation.inputToken == party1Obligation.outputToken
+            && party0Obligation.outputToken == party1Obligation.inputToken;
+        if (!tokenCompatible) {
+            revert IncompatiblePairs();
+        }
+
+        // 2. The input and output amounts must correspond
+        bool amountCompatible = party0Obligation.amountIn == party1Obligation.amountOut
+            && party0Obligation.amountOut == party1Obligation.amountIn;
+        if (!amountCompatible) {
+            revert IncompatibleAmounts();
+        }
     }
 
     /// @notice Authorize an intent in a settlement bundle
