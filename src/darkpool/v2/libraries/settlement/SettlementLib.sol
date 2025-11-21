1f9f44bd3c842bd6853d051fe7f8b2e2928bf3d3
Yun <41171808+sehyunc@users.noreply.github.com>
2025-11-21 13:58:52 -0800

    darkpoolv2: external-match: bounded-match-result: add authorization

--- src/darkpool/v2/libraries/settlement/SettlementLib.sol
+++ src/darkpool/v2/libraries/settlement/SettlementLib.sol
@@ -8,6 +8,7 @@ import { IPermit2 } from "permit2-lib/interfaces/IPermit2.sol";
 import { IVerifier } from "darkpoolv2-interfaces/IVerifier.sol";
 import { DarkpoolConstants } from "darkpoolv2-lib/Constants.sol";
 
+import { BoundedMatchResultBundle } from "darkpoolv2-types/settlement/BoundedMatchResultBundle.sol";
 import {
     PartyId,
     SettlementBundle,
@@ -231,22 +232,26 @@ library SettlementLib {
     }
 
     /// @notice Execute an external settlement bundle
-    /// @param obligation The settlement obligation to validate
-    /// @param settlementBundle The settlement bundle to validate
+    /// @param matchBundle The bounded match result authorization bundle to validate
+    /// @param internalObligation The settlement obligation to validate
+    /// @param internalPartySettlementBundle The settlement bundle for the internal party
     /// @param settlementContext The settlement context to which we append post-validation updates.
     /// @param state The darkpool state containing all storage references
     function executeExternalSettlementBundle(
-        SettlementObligation memory obligation,
-        SettlementBundle calldata settlementBundle,
+        BoundedMatchResultBundle calldata matchBundle,
+        SettlementObligation memory internalObligation,
+        SettlementBundle calldata internalPartySettlementBundle,
         SettlementContext memory settlementContext,
         DarkpoolState storage state,
         IHasher _hasher
     )
         internal
     {
-        SettlementBundleType bundleType = settlementBundle.bundleType;
+        SettlementBundleType bundleType = internalPartySettlementBundle.bundleType;
         if (bundleType == SettlementBundleType.NATIVELY_SETTLED_PUBLIC_INTENT) {
-            NativeSettledPublicIntentLib.execute(obligation, settlementBundle, settlementContext, state);
+            NativeSettledPublicIntentLib.executeBoundedMatch(
+                matchBundle, internalObligation, internalPartySettlementBundle, settlementContext, state
+            );
         } else {
             // TODO: Add support for other settlement bundle types
             revert InvalidSettlementBundleType();
