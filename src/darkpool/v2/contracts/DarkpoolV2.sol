d1b407f693930f2cbbee893d0b4b2a7d8080bc0e
Sehyun Chung <sehyun@renegade.fi>
2026-01-07 18:25:44 -0800

    darkpoolv2: add public order cancellation

--- src/darkpool/v2/contracts/DarkpoolV2.sol
+++ src/darkpool/v2/contracts/DarkpoolV2.sol
@@ -39,6 +39,7 @@ import {
 import { DepositAuth } from "darkpoolv2-types/transfers/Deposit.sol";
 import { WithdrawalAuth } from "darkpoolv2-types/transfers/Withdrawal.sol";
 import { OrderCancellationAuth } from "darkpoolv2-types/OrderCancellation.sol";
+import { PublicIntentPermit } from "darkpoolv2-types/settlement/IntentBundle.sol";
 import { SettlementLib } from "darkpoolv2-lib/settlement/SettlementLib.sol";
 import { ExternalSettlementLib } from "darkpoolv2-lib/settlement/ExternalSettlementLib.sol";
 import { DarkpoolState, DarkpoolStateLib } from "darkpoolv2-lib/DarkpoolState.sol";
@@ -206,7 +207,12 @@ contract DarkpoolV2 is Initializable, Ownable2Step, Pausable, IDarkpoolV2 {
     )
         public
     {
-        StateUpdatesLib.cancelOrder(_state, verifier, auth, orderCancellationProofBundle);
+        StateUpdatesLib.cancelPrivateOrder(_state, verifier, auth, orderCancellationProofBundle);
+    }
+
+    /// @inheritdoc IDarkpoolV2
+    function cancelOrder(OrderCancellationAuth memory auth, PublicIntentPermit calldata permit) public {
+        StateUpdatesLib.cancelPublicOrder(_state, auth, permit);
     }
 
     // --- Deposit --- //
